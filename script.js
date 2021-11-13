d9025b55df9e1bf0fdfe53922165494e7359d5b1
Jlynn209 <jlynn_209@aol.com>
2021-11-12 17:58:33 -0800

    Changed as a visual and also including js.

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
-for(var i = 1; i < 101; i++){
+var elem = document.getElementById("code");
 
-    if(i % 3 == 0 && i % 5 == 0){
-      console.log("FizzBuzz")
-    }
-    else if (i % 3 == 0){
-      console.log("Fizz")
-    }
-    else if (i % 5 == 0){
-      console.log("Buzz")
-    }
-    else
-    {
-      console.log(i)
-    }
+for (var i = 1; i < 101; i++) {
+  var temp = document.createElement("p");
+  temp.id = i;
+  elem.appendChild(temp);
+}
+
+for (var i = 1; i < 101; i++) {
+  var test = document.getElementById(i);
+  if (i % 3 == 0 && i % 5 == 0) {
+    test.innerHTML = "FizzBuzz";
+  } else if (i % 3 == 0) {
+    test.innerHTML = "Fizz";
+  } else if (i % 5 == 0) {
+    test.innerHTML = "Buzz";
+  } else {
+    test.innerHTML = i;
+  }
 }
